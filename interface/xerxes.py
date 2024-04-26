ceb471147451e51fb88b059b26eac805f31d5f96
erfanzar <erfanzare82@yahoo.com>
2024-04-26 18:09:55 +0330

    Updating `interface` and requirements

--- interface/xerxes.py
+++ interface/xerxes.py
@@ -1,7 +1,6 @@
 from agentx.engine_websocket import generate
 import streamlit as st
 
-hostname = ""
 USER_ROLE = "User"
 ASSISTANT_ROLE = "Xerxes"
 USER_AVATAR = "👨‍💻"
@@ -22,7 +21,7 @@ def main():
         "a cutting-edge AI startup. My primary function is to assist users "
         "with their various needs, providing expert-level support in wide range of tasks."
     )
-
+    hostname = st.sidebar.text_input("Hostname", value="")
     temperature = st.sidebar.slider("Temperature", min_value=0.001, max_value=5.0, value=0.4)
     top_p = st.sidebar.slider("Top P", min_value=0.001, max_value=1.0, value=0.95)
     top_k = st.sidebar.slider("Top K", min_value=1, max_value=100, value=50)
@@ -43,33 +42,35 @@ def main():
                 st.markdown(message["content"])
 
     if prompt := st.chat_input("Your Prompt Here ... "):
+        if hostname != "":
+            with st.chat_message(USER_ROLE, avatar=USER_AVATAR):
+                st.markdown(prompt)
+            with st.chat_message(ASSISTANT_ROLE, avatar=ASSISTANT_AVATAR):
+                def _streamer():
+                    conversation = st.session_state.conversation
+                    if len(conversation) > 12:
+                        system = conversation[0]
+                        conversation = conversation[-10:]
+                        conversation = [system, *conversation]
+                    for res in generate(
+                            hostname=hostname,
+                            prompt=prompt,
+                            conversation=conversation,
+                            top_p=top_p,
+                            top_k=top_k,
+                            temperature=temperature,
+                            max_new_tokens=max_new_tokens,
+                            max_sequence_length=max_sequence_length,
+                            stream=True
+                    ):
+                        yield res.response
 
-        with st.chat_message(USER_ROLE, avatar=USER_AVATAR):
-            st.markdown(prompt)
-        with st.chat_message(ASSISTANT_ROLE, avatar=ASSISTANT_AVATAR):
-            def _streamer():
-                conversation = st.session_state.conversation
-                if len(conversation) > 12:
-                    system = conversation[0]
-                    conversation = conversation[-10:]
-                    conversation = [system, *conversation]
-                for res in generate(
-                        hostname=hostname,
-                        prompt=prompt,
-                        conversation=conversation,
-                        top_p=top_p,
-                        top_k=top_k,
-                        temperature=temperature,
-                        max_new_tokens=max_new_tokens,
-                        max_sequence_length=max_sequence_length,
-                        stream=True
-                ):
-                    yield res.response
+                response = st.write_stream(_streamer)
 
-            response = st.write_stream(_streamer)
-
-            st.session_state.conversation.append({"role": "user", "content": prompt})
-            st.session_state.conversation.append({"role": "assistant", "content": response})
+                st.session_state.conversation.append({"role": "user", "content": prompt})
+                st.session_state.conversation.append({"role": "assistant", "content": response})
+        else:
+            st.warning("Please enter a valid Hostname for AgentX ServeEngine")
 
 
 if __name__ == '__main__':
